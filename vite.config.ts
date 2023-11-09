def927eca6365e9359d2961cf54399e3a2b76572
David L Whitehurst <me@dlwhitehurst.com>
2023-11-09 00:05:05 -0500

    removed env middleware placeholders

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const userauthTarget = import.meta.env.VITE_USERAUTH_TARGET // "http://localhost:3004"
-const testTarget = import.meta.env.VITE_TEST_TARGET // http://localhost:3005"
 
 // https://vitejs.dev/config/
 export default defineConfig({
