11d7d0c670f39123e5711438621ff1157e71728b
David L Whitehurst <me@dlwhitehurst.com>
2023-11-16 11:28:36 -0500

    housekeeping

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import BoardMonitor from "./components/BoardMonitor.tsx";
 import BoardAdmin from "./components/BoardAdmin.tsx";
 
 import EventBus from "./common/EventBus";
-import MyComponent from "./components/MyComponent.tsx";
 import RegisterAgent from "./components/RegisterAgent.tsx";
 import RegisterMonitor from "./components/RegisterMonitor.tsx";
 
@@ -175,7 +174,6 @@ class App extends Component<Props, State> {
                         <Route path="/agent" element={<BoardAgent />} />
                         <Route path="/monitor" element={<BoardMonitor />} />
                         <Route path="/admin" element={<BoardAdmin />} />
-                        <Route path="/mycomponent" element={<MyComponent />} />
                         <Route path="/register-agent" element={<RegisterAgent />} />
                         <Route path="/register-monitor" element={<RegisterMonitor />} />
 
