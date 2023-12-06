96363c04a5be227445184ace7efc36de063eda44
David L Whitehurst <me@dlwhitehurst.com>
2023-12-06 00:28:59 -0500

    apache headers, all source

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,8 +1,22 @@
-/**
- * VirtualYou
- * @license Apache-2.0
- * @author David L Whitehurst
- */
+/*
+Copyright 2023 VirtualYou
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+mail.service.ts - AWS SES email service
+@author David L Whitehurst
+
+*/
 
 import { SES } from "@aws-sdk/client-ses";
 
@@ -20,15 +34,7 @@ class MailService {
             secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
         }
     });
-/*
-    private ses = new AWS.SES({
-        region: import.meta.env.VITE_AWS_REGION,
-        credentials: new AWS.Credentials({
-            accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY,
-            secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-        }),
-    });
-*/
+
     // Send Agent Invitation Email
     emailAgent(formData: FormValues) {
         console.log(formData.email);
