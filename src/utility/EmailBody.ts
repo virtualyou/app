9b13469160fc8974a5e7ba8a199c6e3f9b0f5694
David L Whitehurst <me@dlwhitehurst.com>
2023-12-23 17:13:50 -0500

    added hash to return path

--- src/utility/EmailBody.ts
+++ src/utility/EmailBody.ts
@@ -35,7 +35,7 @@ export function getAgentBody(name: string) {
     const md = forge.md.sha256.create();
     md.update(agentMne);
     const dkey = md.digest().toHex();
-    const retLink = "https://app.virtualyou.info" + "/register-agent?ownerid=" + id + "&dkey=" + dkey;
+    const retLink = "https://app.virtualyou.info" + "#/register-agent?ownerid=" + id + "&dkey=" + dkey;
 
     return name + ",\n\nYou have been invited to be an Agent for " + myName + ". Use this link to register and begin helping " + myName + ".\n\n" + retLink;
 }
@@ -54,7 +54,7 @@ export function getMonitorBody(name: string) {
     const md = forge.md.sha256.create();
     md.update(monitorMne);
     const dkey = md.digest().toHex();
-    const retLink = "https://app.virtualyou.info" + "/register-monitor?ownerid=" + id + "&dkey=" + dkey;
+    const retLink = "https://app.virtualyou.info" + "#/register-monitor?ownerid=" + id + "&dkey=" + dkey;
 
     return name + ",\n\nYou have been invited to be a Monitor for " + myName + ". Use this link to register and begin helping " + myName + ".\n\n" + retLink;
 }
